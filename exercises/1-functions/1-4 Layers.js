5ccdbeed97d6278ed77b13bead325104e62ff4d7
Maxim Westerbeek <maxim.westerbeek@student.ehb.be>
2022-10-05 22:50:41 +0200

    1-4 Layers excercise optimisation

--- exercises/1-functions/1-4 Layers.js
+++ exercises/1-functions/1-4 Layers.js
@@ -9,64 +9,25 @@ function drawLine() {
    canvas.height = window.innerHeight;
    let context = canvas.getContext("2d");
 
-   //Layer 1
-   context.fillStyle = 'grey';
-   context.beginPath();
-   context.rect(50, 50, 300, 300);
-   context.fill();
-
+   context.fillStyle = 'lightBlue';
    context.lineWidth = '3';
-   context.strokeStyle = 'black';
+
+   //Layer 1
    context.beginPath();
    context.rect(50, 50, 300, 300);
-   context.stroke();
 
    //Layer 2
-   context.fillStyle = 'grey';
-   context.beginPath();
    context.rect(50, 50, 250, 250);
-   context.fill();
-
-   context.lineWidth = '3';
-   context.strokeStyle = 'black';
-   context.beginPath();
-   context.rect(50, 50, 250, 250);
-   context.stroke();
 
    //Layer 3
-   context.fillStyle = 'grey';
-   context.beginPath();
    context.rect(100, 50, 200, 200);
-   context.fill();
-
-   context.lineWidth = '3';
-   context.strokeStyle = 'black';
-   context.beginPath();
-   context.rect(100, 50, 200, 200);
-   context.stroke();
 
    //Layer 4
-   context.fillStyle = 'grey';
-   context.beginPath();
    context.rect(150, 100, 150, 150);
-   context.fill();
-
-   context.lineWidth = '3';
-   context.strokeStyle = 'black';
-   context.beginPath();
-   context.rect(150, 100, 150, 150);
-   context.stroke();
 
    //Layer 5
-   context.fillStyle = 'grey';
-   context.beginPath();
    context.rect(150, 150, 100, 100);
    context.fill();
-
-   context.lineWidth = '3';
-   context.strokeStyle = 'black';
-   context.beginPath();
-   context.rect(150, 150, 100, 100);
    context.stroke();
-
+   
 }
