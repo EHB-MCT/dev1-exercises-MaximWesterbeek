7b39928ff77054a09171f744cd0a28ea84f7a257
Maxim Westerbeek <maxim.westerbeek@student.ehb.be>
2022-10-05 23:11:45 +0200

    1-5a Circle excercise finished

---
+++ exercises/1-functions/1-5a Circle.js
@@ -0,0 +1,28 @@
+"use strict";
+
+drawLine();
+
+function drawLine() {
+
+   let canvas = document.querySelector("canvas");
+   canvas.width = window.innerWidth;
+   canvas.height = window.innerHeight;
+   let context = canvas.getContext("2d");
+
+   context.fillStyle = 'orange';
+   context.beginPath();
+   context.rect(50, 50, 300, 300);
+   context.fill();
+
+   context.beginPath();
+   context.fillStyle = 'yellow';
+   context.arc(200, 250, 100, 0, 2* Math.PI);
+   context.fill();
+   
+   context.beginPath();
+   context.fillStyle = 'blue';
+   context.rect(50, 250, 300, 100);
+   context.fill();
+
+
+}
