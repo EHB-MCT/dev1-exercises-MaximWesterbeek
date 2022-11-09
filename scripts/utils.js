12653179c5ded266006e21b1ff4e27be6e4977d2
Maxim Westerbeek <maxim.westerbeek@student.ehb.be>
2022-11-09 10:27:22 +0100

    Utils updated

--- scripts/utils.js
+++ scripts/utils.js
@@ -19,10 +19,10 @@ export function drawLine(x1, y1, x2, y2) {
 }
 
 /**
- * Draw a circle
+ * Draw a stroked circle
  * @param {Number} x X coordinate centre
  * @param {Number} y Y coordinate centre
- * @param {Number} radius Radius circle
+ * @param {Number} radius radius circle
  */
 export function strokeCircle(x, y, radius) {
     context.beginPath();
@@ -30,13 +30,27 @@ export function strokeCircle(x, y, radius) {
     context.stroke();
 }
 
+/**
+ * Draw a stroked ellipse
+ * @param {*} x X coordinate centre
+ * @param {*} y Y coordinate centre
+ * @param {*} rX horizontal radius
+ * @param {*} rY vertical radius
+ */
+ export function strokeEllipse(x, y, rX, rY) {
+    context.beginPath();
+    context.ellipse(x, y, rX, rY, 0, 0, Math.PI * 2);
+    context.stroke();
+}
+
 
             //FILLED SHAPES
-/**
- * Draw a filled circle with outline
+
+            /**
+ * Draw a stroked and filled circle
  * @param {Number} x X coordinate centre
  * @param {Number} y Y coordinate centre
- * @param {Number} radius Radius circle
+ * @param {Number} radius radius circle
  */
  export function fillAndStrokeCircle(x, y, radius) {
     context.beginPath();
@@ -49,7 +63,7 @@ export function strokeCircle(x, y, radius) {
  * Draw a filled circle
  * @param {Number} x X coordinate centre
  * @param {Number} y Y coordinate centre
- * @param {Number} radius Radius circle
+ * @param {Number} radius radius circle
  */
  export function fillCircle(x, y, radius) {
     context.beginPath();
@@ -59,11 +73,11 @@ export function strokeCircle(x, y, radius) {
 
 
 /**
- * Draw a filled ellipse with outline
+ * Draw a stoked and filled ellipse
  * @param {Number} x X coordinate centre
  * @param {Number} y Y coordinate centre
- * @param {Number} rX X coordinate width
- * @param {Number} rY Y coordinate height
+ * @param {Number} rX horizontal radius
+ * @param {Number} rY vertical radius
  */
 export function fillAndStrokeEllipse(x, y, rX, rY) {
     context.beginPath();
@@ -76,8 +90,8 @@ export function fillAndStrokeEllipse(x, y, rX, rY) {
  * Draw a filled ellipse
  * @param {Number} x X coordinate centre
  * @param {Number} y Y coordinate centre
- * @param {Number} rX X coordinate width
- * @param {Number} rY Y coordinate height
+ * @param {Number} rX X horizontal radius
+ * @param {Number} rY Y vertical radius
  */
  export function fillEllipse(x, y, rX, rY) {
     context.beginPath();
@@ -94,10 +108,59 @@ export function rgb(r, g, b) {
     return rgb;
 }
 
+export function rgba(r, g, b, a) {
+    let rgb = "rgb(" + r + "," + g + "," + b +","+ a +" %)";
+    return rgb;
+}
+
 export function hsl(h, s, l) {
     return "hsl("+ h +","+ s +"%,"+ l +"%)";
 }
 
 export function hsla(h, s, l, a) {
     return "hsl("+ h +","+ s +"%,"+ l +"%,"+ a +")";
+}
+
+
+            //MATH STUFF
+
+/** function that converts an angle in degrees to radians
+ * @param {number} degrees 
+ */
+ export function degrees(degrees) {
+    return degrees * (Math.PI / 180);
+}
+
+/**
+ * function that calculates the distance between 2 coordinates
+ * @param {number} x1 x coordinate of the first point
+ * @param {number} y1 y coordinate of the first point
+ * @param {number} x2 x coordinate of the second point
+ * @param {number} y2 y coordinate of the second point
+ */
+ export function calculateDistance(x1, y1, x2, y2) {
+    return Math.hypot(x2 - x1, y2 - y1);
+}
+
+/**
+ * function that returns a random whole number between a minimum and a maximumm value
+ * @param {number} min minimum value
+ * @param {number} max maximum value
+ */
+ export function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+ * function that returns a structured random decimal number based on a Gaussian curve
+ * Adapted from stackoverflow answer by Dorian: https://stackoverflow.com/a/39187274
+ */
+ export function randomGaussian() {
+    var rand = 0;
+
+    for (var i = 0; i < 6; i += 1) {
+        rand += Math.random() * 2 - 1;
+    }
+
+    return rand / 6;
 }
